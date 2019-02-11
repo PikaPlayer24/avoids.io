7f5430f5a22e2017eec05e56e1e60219a6c5ebf0
PikaPlayer24 <noahpace124@gmail.com>
2019-02-10 16:27:56 -0800

    Added chat and setup simple movement with node

--- app.js
+++ app.js
@@ -7,27 +7,145 @@ app.get('/',function(req, res) {
     res.sendFile(__dirname + '/client/index.html');
 });
 app.use('/client',express.static(__dirname + '/client'));
- 
-serv.listen(3000);
-console.log("Server started.");
+
+var port = 3000
+serv.listen(port);
+console.log("Server started on port " + port + ".");
+
 
 var SOCKET_LIST = {};
-var PLAYER_LIST = {};
+
+var Entity = function(){
+	var self = {
+		x:250,
+		y:250,
+		spdX:0,
+		spdY:0,
+		id:"",
+	}
+	self.update = function(){
+		self.updatePosition();
+	}
+	self.updatePosition = function(){
+		self.x += self.spdX;
+		self.y += self.spdY;
+	}
+	return self;
+}
+
+var Player = function(param){
+	var self = Entity();
+	self.id = param.id;
+	self.number = "" + Math.floor(10 * Math.random());
+	self.username = param.username;
+	self.pressingRight = false;
+	self.pressingLeft = false;
+	self.pressingUp = false;
+	self.pressingDown = false;
+	self.maxSpd = 10;
+	
+	var super_update = self.update;
+	self.update = function(){
+		self.updateSpd();
+		super_update();
+	}
+	
+	
+	self.updateSpd = function(){
+		if(self.pressingRight)
+			self.spdX = self.maxSpd;
+		else if(self.pressingLeft)
+			self.spdX = -self.maxSpd;
+		else
+			self.spdX = 0;
+		
+		if(self.pressingUp)
+			self.spdY = -self.maxSpd;
+		else if(self.pressingDown)
+			self.spdY = self.maxSpd;
+		else
+			self.spdY = 0;		
+	}
+	Player.list[param.id] = self;
+	return self;
+}
+Player.list = {};
+Player.onConnect = function(socket,username){
+	var player = Player({
+		username:username,
+		id:socket.id,
+	});
+	socket.on('keyPress',function(data){
+		if(data.inputId === 'left')
+			player.pressingLeft = data.state;
+		else if(data.inputId === 'right')
+			player.pressingRight = data.state;
+		else if(data.inputId === 'up')
+			player.pressingUp = data.state;
+		else if(data.inputId === 'down')
+			player.pressingDown = data.state;
+	});
+	socket.on('sendMsgToServer',function(data){
+		for(var i in SOCKET_LIST){
+			SOCKET_LIST[i].emit('addToChat',player.username + ': ' + data);
+		}
+	});
+}
+
+Player.onDisconnect = function(socket){
+	delete Player.list[socket.id];
+}
+Player.update = function(){
+	var pack = [];
+	for(var i in Player.list){
+		var player = Player.list[i];
+		player.update();
+		pack.push({
+			x:player.x,
+			y:player.y,
+			number:player.number
+		});		
+	}
+	return pack;
+}
+
+var DEBUG = true;
 
 var io = require('socket.io')(serv,{});
 io.sockets.on('connection', function(socket){
-    socket.id = Math.random();
-    SOCKET_LIST[socket.id] = socket;
-    console.log(socket.id + " connected.");
- 	
-    socket.on('play',function(data){
-    	console.log(data.name);
-    	socket.emit('playResponse',{success:true});
-    });
+	socket.id = Math.random();
+	SOCKET_LIST[socket.id] = socket;
+	
+	socket.on('play',function(data){
+		Player.onConnect(socket,data.name);
+	});
+	
+	console.log(socket.id + " connected.");
 
-    socket.on('disconnect',function(){
-    	console.log(socket.id + " disconnected.");
-        delete SOCKET_LIST[socket.id];
-    });
-   
+	socket.on('disconnect',function(){
+		console.log(socket.id + " disconnected.");
+		delete SOCKET_LIST[socket.id];
+		Player.onDisconnect(socket);
+	});
+	
+	socket.on('evalServer',function(data){
+		if(!DEBUG)
+			return;
+		var res = eval(data);
+		socket.emit('evalAnswer',res);		
+	});
+	
+	
+	
 });
+
+setInterval(function(){
+	var pack = {
+		player:Player.update(),
+	}
+	
+	for(var i in SOCKET_LIST){
+		var socket = SOCKET_LIST[i];
+		socket.emit('newPositions',pack);
+	}
+},1000/25);
